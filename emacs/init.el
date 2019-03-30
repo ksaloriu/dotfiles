d6db9a948cf88ce4f07d0b8956b15681136ef188
Karri Saloriutta <ksaloriu@gmail.com>
2019-03-30 19:41:20 +0200

    Remove custom-set-variables

--- emacs/init.el
+++ emacs/init.el
@@ -217,15 +217,6 @@
 ;; (require 'ob-python)
 
 ;; (provide 'my-org-conf)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (scala-mode ledger-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
